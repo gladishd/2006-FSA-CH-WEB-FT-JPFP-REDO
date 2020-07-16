e16392b1689d54d26a3b0a398d8de5a5d2357b51
Dean Gladish <gladishd@carleton.edu>
2020-07-16 14:23:33 -0500

    got the 'no students available' logic to work

--- app/components/Campus.js
+++ app/components/Campus.js
@@ -9,11 +9,11 @@ export class Campus extends React.Component {
   componentDidMount() {
     let campusId = this.props.match.params.campusId;
     this.props.getSingleCampus(campusId);
-    console.log(this.props)
   }
   render() {
-    console.log(this.props)
+    // console.log(this.props)
     let { address, description, id, imageUrl, name, students } = this.props.currentCampus;
+    console.log(this.props.currentCampus.students);
     return (
       <div>
         <div className='flex-container'>
@@ -21,6 +21,28 @@ export class Campus extends React.Component {
           <img src={imageUrl} />
           <div> Address: {address}</div>
           <div>Description: {description} </div>
+          {/* don't render students unless they exist
+           * (and they don't exist until the component mounts,
+           * after the first render). */}
+          <div>Students:
+            {this.props.currentCampus.students && this.props.currentCampus.students
+              .map(student => {
+                return <div>
+
+                  Name: {student.firstName} {student.lastName}
+                  <br></br>
+                Email: {student.email}
+                  <br></br>
+                GPA: {student.gpa}
+                  <br></br>
+                  <img src={student.imageUrl} />
+
+                </div>
+
+              })}
+            {this.props.currentCampus.students && Object.keys(this.props.currentCampus.students).length === 0 && ' No Students!'}
+          </div>
+
         </div>
       </div>
     )
