87fba94feb38d40dae55ad419bf1d022eaaf834d
Dean Gladish <gladishd@carleton.edu>
2020-07-18 18:14:15 -0500

    fixed scrolling activating navbar

--- public/scrollHeader.js
+++ public/scrollHeader.js
@@ -1,6 +1,9 @@
+setTimeout(console.log('waiting for page to load...'), 1000)
+
 let header = document.getElementsByTagName("nav")[0];
 
 window.addEventListener("scroll", function () {
+  header = document.getElementsByTagName("nav")[0];
   const scrollPosition = window.scrollY;
   if (scrollPosition > 0) {
     header.classList.add("active");
