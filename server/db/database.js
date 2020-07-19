5d4d398fe6cfc6a18a2810021e43f9770274ac42
Dean Gladish <gladishd@carleton.edu>
2020-07-18 22:47:59 -0500

    editing database connection

--- server/db/database.js
+++ server/db/database.js
@@ -14,8 +14,17 @@ const pkg = require('../../package.json')
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
 console.log(chalk.yellow(`Opening database connection to ${dbName}`))
 
-const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
-  logging: false,
-})
+// const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
+//   logging: false,
+// })
+
+const databaseUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
+const db = new Sequelize(
+  databaseUrl,
+  {
+    logging: false,
+    operatorsAliases: false,
+  }
+)
 
 module.exports = db
